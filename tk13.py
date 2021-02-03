074b74025acdac62c2a0c3663a54e7491d446a4f
NE19001 パリデェ ラメス <ne19001@azec.ac.jp>
2021-02-03 09:27:23 +0900

    add tk??.pyw

---
+++ tk13.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+#tk13.pyw
+import tkinter as tk
+root = tk.Tk()
+root.geometry("300x200")
+lb = tk.Label(text="This is a Label. This is a Label. This is a Label.")
+#メッセージの作成（さくせい）
+ms = tk.Message(text="This is a Message. This is a Message. This is a Message.")
+[widget.pack() for widget in (lb, ms)]
+root.mainloop()
